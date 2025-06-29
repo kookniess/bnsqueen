8522a37c5874e7a6f28a6cb4e146aecc77d0c033
kookniess <151387609+kookniess@users.noreply.github.com>
2025-06-30 09:38:54 +1200

    Update config.js

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -3,7 +3,7 @@
  */
 const CONFIG = {
     PROXIO_WELCOME_COVER_ENABLE: true, //是否显示页面进入的欢迎文字
-    PROXIO_WELCOME_TEXT: '欢迎来到《玻尿酸女王那些事儿》官方网站，点我进入', // 欢迎文字，留空则不启用
+    PROXIO_WELCOME_TEXT: '《玻尿酸女王那些事儿》官方网站', // 欢迎文字，留空则不启用
 
     // 英雄区块导航
     PROXIO_HERO_ENABLE: true, // 开启英雄区
