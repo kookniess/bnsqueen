29d3da5ea38d07a8d9f433e633e8030de0e6de7d
kookniess <151387609+kookniess@users.noreply.github.com>
2025-07-05 15:41:26 +1200

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到《玻尿酸女王那些事儿》的世界', url: 'https://bnsqueen.com' },
+    { title: '来这里追短剧，听幕后，读原著，看证据', url: 'https://youtube.com/@bnsqueen_com' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -18,19 +18,19 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
+  HEO_HERO_TITLE_1: '看证据',
+  HEO_HERO_TITLE_2: '看华熙秘密',
   HEO_HERO_TITLE_3: 'bnsqueen.com',
   HEO_HERO_TITLE_4: '欢迎点赞，分享加小铃铛',
   HEO_HERO_TITLE_5: 'Youtube和X推特上线',
   HEO_HERO_TITLE_LINK: 'https://youtube.com/@bnsqueen_com',
   // 英雄区遮罩文字
-  HEO_HERO_COVER_TITLE: '这些不是一份文件，而是一段真相的印记',
+  HEO_HERO_COVER_TITLE: '不是一份文件，而是真相的印记',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '1必看精选', url: '/tag/必看精选' },
+  HEO_HERO_CATEGORY_2: { title: '1热门文章', url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_3: { title: '1实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
