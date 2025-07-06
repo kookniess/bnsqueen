18f23ef078fbe7172fdeec5799c613ee1ff6b056
kookniess <151387609+kookniess@users.noreply.github.com>
2025-07-06 23:08:45 +1200

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -28,9 +28,9 @@ const CONFIG = {
   HEO_HERO_COVER_TITLE: '证据我还留存着！',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '公益基金', url: 'cfund' },
-  HEO_HERO_CATEGORY_2: { title: '关于我', url: 'about' },
-  HEO_HERO_CATEGORY_3: { title: '支持我', url: 'support' },
+  HEO_HERO_CATEGORY_1: { title: '公益基金', url: '/cfund' },
+  HEO_HERO_CATEGORY_2: { title: '关于我', url: '/about' },
+  HEO_HERO_CATEGORY_3: { title: '支持我', url: '/support' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '',
