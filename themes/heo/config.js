3684c0bee0124bf5a4ba973ec60686105835ec55
kookniess <151387609+kookniess@users.noreply.github.com>
2025-07-06 15:11:27 +1200

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -25,7 +25,7 @@ const CONFIG = {
   HEO_HERO_TITLE_5: '',
   HEO_HERO_TITLE_LINK: '',
   // 英雄区遮罩文字
-  HEO_HERO_COVER_TITLE: '我还留存着！',
+  HEO_HERO_COVER_TITLE: '证据我还留存着！',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '追短剧', url: '/post/video' },
@@ -33,7 +33,7 @@ const CONFIG = {
   HEO_HERO_CATEGORY_3: { title: '读原著', url: '/post/novel' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HEO_HERO_RECOMMEND_POST_TAG: '推荐',
+  HEO_HERO_RECOMMEND_POST_TAG: '',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
@@ -51,7 +51,7 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/kookniess',
+  HEO_INFO_CARD_URL2: '/support',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
   HEO_INFO_CARD_URL3: 'https://www.bnsqueen.com',
   HEO_INFO_CARD_TEXT3: '了解更多',
