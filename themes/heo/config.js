ffa62e2b4dc9cbdd65975774cff58d2e55625a8e
kookniess <151387609+kookniess@users.noreply.github.com>
2025-07-24 01:39:22 +1200

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -30,7 +30,7 @@ const CONFIG = {
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '我的声明', url: '/statements' },
   HEO_HERO_CATEGORY_2: { title: '内幕揭秘', url: '/evidence' },
-  HEO_HERO_CATEGORY_3: { title: '收听故事：58集播客', url: '/post/podcast' },
+  HEO_HERO_CATEGORY_3: { title: '最新动态', url: '/latest' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '',
